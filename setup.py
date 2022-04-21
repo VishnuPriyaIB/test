38e6b8e80bef7e948dd363c71df62ec307270972
VishnuPriyaIB <vishnu.priya@innoboon.com>
2022-04-21 13:56:40 +0530

    test

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='test',
-    version='0.0.1',
+    name='Vishnupriya',
+    version='0.0.2',
     author='VP',
     author_email='vishnu.priya@innoboon.com',
     description='Testing installation of Package',
     long_description=long_description,
     url='https://github.com/VishnuPriyaIB/test',
     license='MIT',
-    packages=['test'],
+    packages=[],
 )
